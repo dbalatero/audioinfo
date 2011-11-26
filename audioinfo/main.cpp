10215ab876d85bd399f1cbdb039364676f45aa6d
David Balatero <dbalatero@gmail.com>
2011-11-25 16:06:56 -0800

    Initial Commit

---
+++ audioinfo/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  audioinfo
+//
+//  Created by David Balatero on 11/25/11.
+//  Copyright 2011 MediaPiston. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
